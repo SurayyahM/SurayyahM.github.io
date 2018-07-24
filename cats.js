4db2ddb5bbf3d9b025b8d5cad84810910eab08e7
unknown <mitchellsurayyah23@gmail.com>
2018-07-24 15:18:37 -0400

    My first commit

---
+++ cats.js
@@ -0,0 +1,10 @@
+alert(img src=floatingcat.jpg);
+console.log("test");
+
+images=["https://peopledotcom.files.wordpress.com/2017/12/smush-the-cat-1.jpg",
+        "http://a57.foxnews.com/images.foxnews.com/content/fox-news/lifestyle/2017/11/09/how-to-keep-cat-from-scratching-your-sofa-to-shreds/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1510172827500.jpg?ve=1&tl=1&text=big-top-image",
+        "https://static.designboom.com/wp-content/uploads/2018/04/cat-VR-isobar-designboom-01.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_-CPXu4l97NwIiHFm6A38JG42O8EM-7liczCj4WQiFA1f8kPn",
+        "https://s-i.huffpost.com/gen/4140832/images/o-A-DOG-AND-A-CAT-facebook.jpg",
+        "https://cdn.shopify.com/s/files/1/0344/6469/products/New-Cute-Pet-Cat-Costume-Cartoon-Funny-Pet-Cat-Clothes-Hot-Puppy-Clothing-Doctor-Uniform-Hat_copy_1024x1024.jpg?v=1504800100"
+      ];
